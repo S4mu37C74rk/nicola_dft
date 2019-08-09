d680582c7431d6bd57736db15379d60ec3c155bf
Samuel Clark <sc2195@david>
2019-08-09 13:52:38 +0100

    Add results and tools for integrating metadata

---
+++ dft_meta.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 01 16:28:26 2019
+
+@author: sc2195
+"""
+import csv
+
+    
+lines = []
+
+with open('6-311g**_complete.csv', 'r') as db:
+     reader = csv.reader(db)
+     for row in reader:
+         alpha, beta, function = None, None, None
+         with open('nicola/_{:}.metadata'.format(row[0]), 'r') as f:
+              lineList = f.readlines()
+              dataList = [i.split('     ') for i in lineList]
+              for d in dataList:
+     	           if d[0] == 'ExpBeta':
+     	                beta = float(d[1].strip())
+     	           elif d[0] == 'ExpAlpha':
+     	                alpha = float(d[1].strip())
+     	           elif 'OldClassType' in d[0]:
+     	                function = d[0].strip().split('  ')[2]
+         
+         row.extend([alpha, beta, function])
+         lines.append(row)
+     db.close()
+
+with open('6-311g**_complete.csv', 'w') as writeFile:
+     writer = csv.writer(writeFile)
+     writer.writerows(lines)
+     writeFile.close()
